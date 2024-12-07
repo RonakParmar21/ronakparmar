575c77f52143fe11a8f8eedc22924f3d463866af
Ronak Parmar <parmarronak21112003@gmail.com>
2024-12-07 20:55:58 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import "./app.scss";
-import Contact from "./components/contact/Contact";
-import Cursor from "./components/cursor/Cursor";
-import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/Navbar";
-import Parallax from "./components/parallax/Parallax";
-import Projects from "./components/projects/Projects";
-import Services from "./components/services/Services";
+import React from 'react'
 
 const App = () => {
   return (
     <div>
-      {/* <Cursor /> */}
-      <section id="Home">
-        <Navbar />
-        <Hero />
-      </section>
-      {/* <section id="Services"><Parallax type="services" /></section> */}
-      <section id="Portfolio"><Services /></section>
-      {/* <section><Parallax type="portfolio" /></section> */}
-        <Projects />
-      <section id="Cotact"><Contact /></section>
-
-      {/* <Test /> */}
-      
+      <h1>Coming Soon...</h1>
+      <p>Site is under production</p>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
